52b07d404da08ca1b52e4ae6b3bc4041950c5f1e
Bruno Santeramo <bruno.santeramo@gmail.com>
2016-01-03 03:57:25 +0100

    added 42.pl

---
+++ src/pif/checkers/42/pif_ip_checker.py
@@ -0,0 +1,35 @@
+#from __future__ import print_function
+
+__title__ = 'pif.checkers.42.pl.pif_ip_checker'
+__author__ = 'Bruno Santeramo'
+__copyright__ = 'Copyright (c) 2016 Bruno Santeramo'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Ip42IPChecker',)
+
+#import re
+
+from requests import get
+
+from pif.base import BasePublicIPChecker, registry
+
+class Ip42IPChecker(BasePublicIPChecker):
+    """
+    Checks IPs using ipecho.net.
+    """
+    uid = 'ip.42.pl'
+
+    def get_public_ip(self):
+        """
+        Gets public IP.
+
+        :return str:
+        """
+        try:
+            data = get('http://ip.42.pl/raw').text
+            return data
+        except Exception as e:
+            if self.verbose:
+                print(e)
+
+
+registry.register(Ip42IPChecker)
