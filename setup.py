d10ec4b437cc95d4aa6355004f6ab9b68fea9514
Artur Barseghyan <artur.barseghyan@gmail.com>
2016-11-19 02:31:37 +0100

    prepare 0.7.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ try:
 except:
     readme = ''
 
-version = '0.7.3'
+version = '0.7.4'
 
 exec_dirs = [
     'src/pif/bin/',
@@ -17,11 +17,11 @@ for exec_dir in exec_dirs:
     execs += [os.path.join(exec_dir, f) for f in os.listdir(exec_dir)]
 
 setup(
-    name = 'pif',
-    version = version,
-    description = ("Public IP address checker."),
+    name='pif',
+    version=version,
+    description="Public IP address checker.",
     long_description=readme,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
@@ -29,22 +29,23 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
         "Topic :: Internet"
     ],
-    keywords = 'public ip checker, ip',
-    author = 'Artur Barseghyan',
-    author_email = 'artur.barseghyan@gmail.com',
-    url = 'https://github.com/barseghyanartur/pif',
-    package_dir = {'':'src'},
-    packages = find_packages(where='./src'),
-    include_package_data = True,
-    package_data = {
+    keywords='public ip checker, ip',
+    author='Artur Barseghyan',
+    author_email='artur.barseghyan@gmail.com',
+    url='https://github.com/barseghyanartur/pif',
+    package_dir={'': 'src'},
+    packages=find_packages(where='./src'),
+    include_package_data=True,
+    package_data={
         'pif': execs,
     },
-    scripts = ['src/pif/bin/get-public-ip',],
-    license = 'GPL 2.0/LGPL 2.1',
-    install_requires = ['requests>=1.2.3', 'argparse',]
+    scripts=['src/pif/bin/get-public-ip',],
+    license='GPL 2.0/LGPL 2.1',
+    install_requires=['requests>=1.2.3', 'argparse']
 )
--- src/pif/__init__.py
+++ src/pif/__init__.py
@@ -1,8 +1,8 @@
 from pif.utils import get_public_ip
 
 __title__ = 'pif'
-__version__ = '0.7.3'
-__build__ = 0x00000a
+__version__ = '0.7.4'
+__build__ = 0x00000b
 __author__ = 'Artur Barseghyan'
 __copyright__ = 'Copyright (c) 2013-2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
